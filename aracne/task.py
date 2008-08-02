3956d6eddcfd51df868fb1a75fc7702635d145a3
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2008-08-02 02:10:43 -0400

    Implemented simple TaskQueue for testing.
    Implemented a simple TaskQueue (using Queue.Queue, ibdem that the ResultQueue)
    to allow testing the application.

--- aracne/task.py
+++ aracne/task.py
@@ -15,6 +15,10 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import Queue
+
+from aracne.errors import EmptyQueueError
+
 
 class CrawlTask(object):
     """Crawl task.
@@ -34,4 +38,27 @@ class TaskQueue(object):
     def __init__(self, config):
         """Initialize attributes.
         """
-        self._config = config
+        self._queue = Queue.Queue()
+
+    def put(self, item):
+        """Put an item into the queue.
+        """
+        self._queue.put(item)
+
+    def get(self, block=True, timeout=None):
+        """Remove and return an item from the queue.
+
+        If the optional args `block` is `True` and `timeout` is `None` (the
+        default), block if necessary until an item is available. If `timeout`
+        is a positive number, it blocks at most `timeout` seconds and raises
+        the `EmptyQueueError` exception if no item was available within that
+        time.  Otherwise (`block` is `False`), return an item if one is
+        immediately available, else raise the `EmptyQueueError` exception
+        (`timeout` is ignored in that case).
+        """
+        try:
+            item = self._queue.get(block, timeout)
+        except Queue.Empty:
+            raise EmptyQueueError()
+        else:
+            return item
