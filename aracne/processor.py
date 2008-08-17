b2835a6dcd8d1fc9b73f5614384b430d928f9ebb
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2008-08-17 00:07:33 -0400

    Modified ProcessorManager.
    Modified the implementation of the ProcessorManager to sleep if no crawl result
    is available.  Changed ResultQueue to support this modification.

--- aracne/processor.py
+++ aracne/processor.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import time
 import threading
 
 from aracne.errors import EmptyQueueError
@@ -27,14 +28,12 @@ class ResultProcessor(object):
     results from the `ResultQueue`.
     """
 
-    def __init__(self, task_queue, result_queue):
+    def __init__(self, config, tasks, results):
         """Initialize instances.
 
         The `ProcessorManager` will create an instance of this class providing
         the `TaskQueue` and the `ResultQueue` as arguments.
         """
-        self._task_queue = task_queue
-        self._result_queue = result_queue
 
     def process(self, result):
         """Process the crawl result.
@@ -44,43 +43,42 @@ class ResultProcessor(object):
 class ProcessorManager(threading.Thread):
     """Processor manager.
 
-    Creates, manages and feeds the `ResultProcessor` with the results
+    Creates, manages and feeds the selected `ResultProcessor` with the results
     (`CrawlResult`) received from the `ResultQueue`.  It runs in an independent
     thread of execution.
     """
 
-    def __init__(self, config, task_queue, result_queue):
-        """Initializes attributes.
+    def __init__(self, config, tasks, results):
+        """Initializes processor.
         """
         threading.Thread.__init__(self)
-        self._processor = ResultProcessor(task_queue, result_queue)
-        self._sleep_time = config['sleep']
-        self._task_queue = task_queue
-        self._result_queue = result_queue
+        self._config = config
+        self._tasks = tasks
+        self._results = results
+        # TODO: Create the configured processor.
+        self._processor = ResultProcessor(config, tasks, results)
         self._running = False
         self._running_lock = threading.Lock()
 
     def run(self):
         """Runs the processor manager.
 
-        Sets the running flag and then enters a loop processing the results
-        (`CrawlResults`) until the flag is cleared.
+        Sets the running flag and then enters a loop processing crawl results
+        (`CrawlResult`) until the flag is cleared.
         """
         self._running_lock.acquire()
         self._running = True
         while self._running:
             self._running_lock.release()
             try:
-                # Try to get a crawl result to be processed.  Wait a few
-                # seconds for an item if not available right now.
-                result = self._result_queue.get(timeout=self._sleep_time)
+                # Try to get a crawl result to process.  If there is not result
+                # available the thread should sleep.
+                result = self._results.get()
             except EmptyQueueError:
-                # Crawl result not available.  Check the running flag and if
-                # not cleared try to get another crawl result.
-                pass
+                time.sleep(self._config['sleep'])
             else:
-                # Crawl result available.
                 self._processor.process(result)
+                # TODO: Release the result from the queue.
             self._running_lock.acquire()
         self._running_lock.release()
 
