34a9d89e8cffade59e5359a43956a02f6870ea13
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2008-08-18 02:11:10 -0400

    Added support to create unittest for the source code.

---
+++ tests/testresultqueue.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008 Yasser González Fernández <yglez@uh.cu>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import sys
+import optparse
+import unittest
+
+
+def main():
+    """Run test in this module
+    """
+    parser = optparse.OptionParser()
+    parser.add_option('-v', dest='verbosity', default='1',
+                      type='choice', choices=['0', '1', '2'],
+                      help='verbosity level: 0 = minimal, 1 = normal, 2 = all')
+    options, args = parser.parse_args()
+    testdir = os.path.dirname(os.path.abspath(__file__))
+    srcdir = os.path.abspath(os.path.join(testdir, os.path.pardir))
+    sys.path.append(srcdir)
+    suite = unittest.TestLoader().loadTestsFromModule('__main__')
+    runner = unittest.TextTestRunner(verbosity=options.verbosity)
+    result = runner.run(suite)
+    sys.exit(not result.wasSuccessful())
+
+
+if __name__ == '__main__':
+    main()
