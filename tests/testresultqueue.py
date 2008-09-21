82d2ed5641065057aae751196f97885b9255f5d6
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2008-09-21 02:17:29 -0400

    Modified ResultQueue test case.
    Added new sites to the ResultQueue test case and other minor modifications.

--- tests/testresultqueue.py
+++ tests/testresultqueue.py
@@ -44,10 +44,25 @@ class TestResultQueue(unittest.TestCase):
                 {'url': URL('ftp://andromeda.uh.cu/')},
             'aa958756e769188be9f76fbdb291fe1b2ddd4777':
                 {'url': URL('ftp://deltha.uh.cu/')},
+            'd4af25db08f5fb6e768db027d51b207cd1a7f5d0':
+                {'url': URL('ftp://anduin.uh.cu/')},
+            '886b46f54bcd45d4dd5732e290c60e9639b0d101':
+                {'url': URL('ftp://tigris.uh.cu/')},
+            'ee5b017839d97507bf059ec91f1e5644a30b2fa6':
+                {'url': URL('ftp://lara.uh.cu/')},
+            '341938200f949daa356e0b62f747580247609f5a':
+                {'url': URL('ftp://nimbo.uh.cu/')},
+            'd64f2fc98d015a43da3be34668341e3ee6f79133':
+                {'url': URL('ftp://liverpool.reduh.uh.cu/')},
+            '0d3465f2b9fd5cf55748797c590ea621e3017a29':
+                {'url': URL('ftp://london.reduh.uh.cu/')},
+            'c5bcce5953866b673054f8927648d634a7237a9b':
+                {'url': URL('ftp://bristol.reduh.uh.cu/')},
         }
         self._results = []
+        results_per_site = 10
         for site_id, info in self._sites_info.iteritems():
-            for name in (str(n) for n in xrange(10)):
+            for name in (str(n) for n in xrange(results_per_site)):
                 task = CrawlTask(site_id, info['url'].join(name))
                 self._results.append(CrawlResult(task))
         self._queue = ResultQueue(self._dirname, self._sites_info)
@@ -78,16 +93,13 @@ class TestResultQueue(unittest.TestCase):
         for result in self._results:
             self._queue.put(result)
         self._queue.close()
-        # Remove a site from the list and open the queue again.  It should not
-        # return results from this site but keep the order of the others.
+        # It should not return results from the removed site.
         del self._sites_info[self._sites_info.keys()[0]]
         self._queue = ResultQueue(self._dirname, self._sites_info)
-        for result in self._results:
-            if self._sites_info.has_key(result.task.site_id):
-                returned = self._queue.get()
-                self.assertEquals(returned.task.site_id, result.task.site_id)
-                self.assertEquals(str(returned.task.url), str(result.task.url))
-                self._queue.report_done(returned)
+        while self._queue:
+            returned = self._queue.get()
+            self.assertTrue(returned.task.site_id in self._sites_info)
+            self._queue.report_done(returned)
 
     def tearDown(self):
         if os.path.isdir(self._dirname):
