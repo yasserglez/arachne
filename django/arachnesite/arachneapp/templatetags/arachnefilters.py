c955471cba80484d693775bdee9c8030bc17449e
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2009-02-18 01:38:14 -0500

    Modified the results page to show a link for each part of the path.

---
+++ django/arachnesite/arachneapp/templatetags/arachnefilters.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+#
+# Arachne, a search engine for files and directories.
+# Copyright (C) 2008, 2009 Yasser González Fernández <yglez@uh.cu>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Custom filters for the Arachne application.
+"""
+
+import re
+
+from django import template
+from django.utils.html import escape
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
+
+
+register = template.Library()
+
+
+_URLIZEPARTS_RE = re.compile(r'^(?P<root>[a-zA-Z0-9+-.]+://[^/]*)(?P<dirname>/|/.*/)(?P<basename>[^/]+)$')
+
+
+@register.filter
+@stringfilter
+def urlize_dirname(url, title='Go to directory'):
+    """Create an HTML link for each path component of the dirname of the URL.
+    """
+    LINK = '<a href="%%s" title="%s">%%s</a>' % title
+    try:
+        parts = []
+        match = _URLIZEPARTS_RE.match(url)
+        cur_url = match.group('root')
+        parts.append(LINK % (escape(cur_url), escape(cur_url)))
+        for part in filter(None, match.group('dirname').split('/')):
+            cur_url += '/' + part
+            parts.append(LINK % (escape(cur_url), escape(part)))
+        resp = '/'.join(parts)
+    except IndexError:
+        resp = LINK % (escape(url), escape(url))
+    return mark_safe(resp)
+
+
+@register.filter
+@stringfilter
+def urlize_basename(url, title='Go to directory'):
+    """Create an HTML link for the basename of the URL.
+    """
+    LINK = '<a href="%%s" title="%s">%%s</a>' % title
+    try:
+        match = _URLIZEPARTS_RE.match(url)
+        resp = LINK % (escape(url), escape(match.group('basename')))
+    except IndexError:
+        resp = LINK % (escape(url), escape(url))
+    return mark_safe(resp)
