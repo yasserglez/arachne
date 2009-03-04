6c601ac3318acf1afcca599a98e6d03fd2c6c08c
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2009-03-03 22:57:24 -0500

    Added JavaScript code to toggle site selection.

--- django/arachnesite/arachneapp/media/uh/functions.js
+++ django/arachnesite/arachneapp/media/uh/functions.js
@@ -1,17 +1,30 @@
 function focusQuery() {
-    var query = document.getElementById('query');
+    var query = document.getElementById("query");
     query.focus()
 }
 
 window.onload = focusQuery;
 
 function previous() {
-    var previousform = document.getElementById('previousform');
+    var previousform = document.getElementById("previousform");
     previousform.submit();
-
 }
 
 function next() {
-    var nextform = document.getElementById('nextform');
+    var nextform = document.getElementById("nextform");
     nextform.submit();
+}
+
+function changeSites(action) {
+    var container = document.getElementById("sites");
+    var checkboxes = container.getElementsByTagName("input");
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].type == "checkbox") {
+            if (action < 0) {
+                checkboxes[i].checked = !checkboxes[i].checked;
+            } else {
+                checkboxes[i].checked = action;
+            }
+        }
+    }
 }
