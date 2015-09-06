8160e860a072d603892eb2d8db4c3c8925c911fe
Yasser Gonzalez <yasserglez@gmail.com>
2015-09-05 23:56:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Arachne, a search engine for files and directories.
-# Copyright (C) 2008, 2009, 2010 Yasser González Fernández <ygonzalezfernandez@gmail.com>
-# Copyright (C) 2008, 2009, 2010 Ariel Hernández Amador <gnuaha7@gmail.com>
-#
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from distutils.core import setup
 
@@ -26,18 +9,14 @@ from arachne import __version__
 setup(name='Arachne',
       version=__version__,
       license='GNU General Public License version 3 or any later version.',
-      description='Search engine for files and directories.',
-      long_description='Arachne is a search engine for files and directories' \
-          + ' shared via FTP and similar\nprotocols in a networked environment.',
+      description='Search engine for files shared via FTP and similar protocols.',
       platforms=['POSIX'],
       packages=['arachne', 'arachne.util'],
       package_dir={'arachne': 'arachne'},
       data_files=[
         ('/etc/init.d/', ['data/arachned']),
         ('/etc/arachne/', ['data/daemon.conf', 'data/sites.conf']),
-        ('/usr/share/doc/arachne/', ['AUTHORS.txt', 'INSTALL.txt',
-                                     'LICENSE.txt', 'README.txt',
-                                     'THANKS.txt']),
+        ('/usr/share/doc/arachne/', ['LICENSE.txt', 'README.md', 'THANKS.md']),
         # The arachned script should be copied to the /usr/sbin/ directory.
         ('/usr/sbin/', ['scripts/arachned']),
         # Empty directories required by the default configuration.
